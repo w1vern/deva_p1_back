388ad96a8663baee8ac27e5087fd357a84e8ad05
w1vern <bndvvl@gmail.com>
2025-04-18 15:12:14 +0300

    log

--- back/broker.py
+++ back/broker.py
@@ -1,12 +1,17 @@
 
 from faststream.rabbit import RabbitBroker, fastapi, RabbitQueue, RabbitMessage
 
+from back.config import Config
 from config import settings
 
 from deva_p1_db.schemas.task import TaskToAi, TaskToBack
 from deva_p1_db.enums.rabbit import RabbitQueuesToBack
 
 from database.redis import RedisType, get_redis_client
+
+import logging
+
+
 
 RABBIT_URL = f"amqp://{settings.rabbit_user}:{settings.rabbit_password}@{settings.rabbit_ip}:{settings.rabbit_port}/"
 
@@ -20,8 +25,10 @@ def get_broker() -> RabbitBroker:
 async def send_message(broker: RabbitBroker, queue: RabbitQueue | str, data: TaskToAi | dict):
     await broker.publish(data, queue)
 
+logger = logging.getLogger(__name__)
 
 @router.subscriber(RabbitQueuesToBack.done_task)
 async def handle_done_task(msg: TaskToBack):
+    logger.info(f"task done: {msg}")
     redis = await get_redis_client()
-    await redis.set(f"{RedisType.task}:{msg.task_id}", msg.done, ex=300)
+    await redis.set(f"{RedisType.task}:{msg.task_id}", msg.done, ex=Config.redis_task_status_lifetime)
