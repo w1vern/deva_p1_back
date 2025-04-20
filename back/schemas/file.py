657ba31a57c6e9b1a57d8dbec71eae05330991f6
w1vern <bndvvl@gmail.com>
2025-04-21 00:25:30 +0300

    dates file to schema

--- back/schemas/file.py
+++ back/schemas/file.py
@@ -4,20 +4,39 @@ from typing import Optional
 from pydantic import BaseModel
 from deva_p1_db.models import File
 
+
 class FileSchema(BaseModel):
     id: str
     name: str
+    file_type: str
+    created_date: str
+    last_modified_date: str
     download_url: Optional[str] = None
 
     @classmethod
     def from_db(cls, file: File):
-        return cls(id=str(file.id), name=file.user_file_name)
-    
+        return cls(id=str(file.id), 
+                   file_type=file.file_type, 
+                   name=file.user_file_name,
+                   created_date=file.created_date.isoformat(),
+                   last_modified_date=file.last_modified_date.isoformat()
+                   )
+
+
 class FileDownloadURLSchema(BaseModel):
     id: str
     name: str
+    file_type: str
+    created_date: str
+    last_modified_date: str
     download_url: str
 
     @classmethod
-    def from_db(cls, file: File, url: str):
-        return cls(id=str(file.id), name=file.user_file_name, download_url=url)
+    def from_db(cls, file: File, download_url: str):
+        return cls(id=str(file.id), 
+                   file_type=file.file_type, 
+                   name=file.user_file_name,
+                   created_date=file.created_date.isoformat(),
+                   last_modified_date=file.last_modified_date.isoformat(),
+                   download_url=download_url
+                   )
