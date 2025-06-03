9259382d9ee91baed0855f9f2b8c27bfa0120b05
w1vern <bndvvl@gmail.com>
2025-06-03 11:56:30 +0300

    fix note deps

--- back/depends/get_note.py
+++ back/depends/get_note.py
@@ -8,7 +8,6 @@ from fastapi import Depends
 from back.exceptions import NoteNotFoundException
 
 from .database import get_note_repo
-from .get_file import get_file
 from .get_project import (get_invited_user_repo, get_project_editor,
                           get_project_viewer)
 from .get_user import get_user_db
@@ -23,7 +22,7 @@ async def get_note(note_id: UUID,
     return note
 
 
-async def get_note_viewer(note: Note = Depends(get_file),
+async def get_note_viewer(note: Note = Depends(get_note),
                           user: User = Depends(get_user_db),
                           iur: InvitedUserRepository = Depends(
                               get_invited_user_repo)
@@ -31,7 +30,7 @@ async def get_note_viewer(note: Note = Depends(get_file),
     return await get_project_viewer(note.file.project, user)
 
 
-async def get_note_editor(note: Note = Depends(get_file),
+async def get_note_editor(note: Note = Depends(get_note),
                           user: User = Depends(get_user_db)
                           ) -> User:
     return await get_project_editor(note.file.project, user)
